41df1e69c260c79cdcca84415b27434b786d2b7a
Bibek Subedi <100453794+BbekSubed@users.noreply.github.com>
2022-09-14 19:10:06 +0545

    Update request.js

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 
-// const baseURL = 'https://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+const baseURL = 'https://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
 
 // fetch(baseURL)
 //     .then((data) => { console.log('response', data.json()) })
@@ -9,7 +9,7 @@ const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 //     });
 
 const requestCity = async (city) => {
-    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
+    //const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
